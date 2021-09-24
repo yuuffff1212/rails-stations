94f99c7c0fb664bf79d36135e39372bc54cb4138
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-09-24 20:05:05 +0900

    station6

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,13 +2,28 @@
 
 class Movie < ApplicationRecord
   validates :name, uniqueness: true
+  validates :is_showing, inclusion: { in: [true, false] }
 
-  # with_options presence: true do
-  #   validates :name
-  #   validates :description
-  #   validates :image_url
-  #   validates :year
-  # end
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image_url
+    validates :year
+  end
+
+  def self.search(search)
+    if search != ''
+      Movie.where(['name LIKE(?) OR description LIKE(?)', "%#{search}%", "%#{search}%"])
+    else
+      Movie.all
+    end
+
+    if search != ''
+      Movie.where(is_showing: search([:is_showing]))
+    else
+      Movie.all
+    end
+  end
 
   mount_uploader :image_url, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'movies#index'
-  resources :movies, only: :index
+  resources :movies, only: :index do
+  end
 
   namespace :admin do
     resources :movies, only: %i[index new create edit update destroy]
